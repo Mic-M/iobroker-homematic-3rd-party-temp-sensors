7191ff9127f5fc23756bfec9b7224c2549e73396
Mic-M <39859059+Mic-M@users.noreply.github.com>
2019-03-03 00:07:36 +0100

    Version 0.3 – for Homematic IP thermostats only!

--- script_homematic-3rd-party-temp-sensors.js
+++ script_homematic-3rd-party-temp-sensors.js
@@ -1,8 +1,12 @@
 /*******************************************************************************
  * ---------------------------
  * Script "Einbindung Fremd-Temperatur-Sensoren in Homematic mittels Offset-Setzen"
+ * ----------------------------------------------------
+ * WICHTIG: Funktioniert nur mit HM IP Thermostaten!
+ * Nicht-IP-Thermostate: siehe Version 0.2
+ * ----------------------------------------------------
  * ---------------------------
- * Version: 0.2
+ * Version: 0.3
  * Source: https://github.com/Mic-M/iobroker.homematic-3rd-party-temp-sensors
  ******************************************************************************/
 
@@ -21,21 +25,19 @@
 // 4., 5., usw.: Hier können weitere HomeMatic-Thermostate des Raumes hinzugefügt werden (wie unter 3.).
 //               Dies kann beliebig erweitert werden, also auch 10 oder mehr Thermostate pro Raum möglich.
 const roomsHardware = [];
-//                   [0] Raumname          [1] Datenpunkt zu externen Temperatursensor               [2] Min-Temp   [3] State 1. HM-Thermostat   [4] State 2. HM-Thermostat   [5] State 3. HM-Thermostat   [6] State 4. HM-Thermostat
-roomsHardware[0] = ['Badezimmer',       'mihome.0.devices.weather_v1_xxxxxxxxxxxxxx.temperature',  '20',          'hm-rpc.0.XXXXXXXXXX.4',     '',                          '',                          ''];
-roomsHardware[1] = ['Wohnzimmer',       'mihome.0.devices.weather_v1_xxxxxxxxxxxxxx.temperature',  '21',          'hm-rpc.0.XXXXXXXXXX.4',     'hm-rpc.0.XXXXXXXXXX.4',     '',                          ''];
-roomsHardware[2] = ['Schlafzimmer',     'mihome.0.devices.weather_v1_xxxxxxxxxxxxxx.temperature',  '17',          'hm-rpc.0.XXXXXXXXXX.4',     '',                          '',                          ''];
+//                 [0] Raumname          [1] Datenpunkt zu externen Temperatursensor               [2] Min-Temp   [3] State 1. HM-Thermostat   [4] State 2. HM-Thermostat   [5] State 3. HM-Thermostat   [6] State 4. HM-Thermostat
+roomsHardware[0] = ['Badezimmer',       'mihome.0.devices.weather_v1_xxxxxxxxxxxxxx.temperature',  '20',          'hm-rpc.0.xxxxxxxxxxxxxx.1',     '',                          '',                          ''];
+roomsHardware[1] = ['Wohnzimmer',       'mihome.0.devices.weather_v1_xxxxxxxxxxxxxx.temperature',  '21',          'hm-rpc.0.xxxxxxxxxxxxxx.1',     'hm-rpc.0.xxxxxxxxxxxxxx.1', '',                          ''];
+roomsHardware[2] = ['Schlafzimmer',     'mihome.0.devices.weather_v1_xxxxxxxxxxxxxx.temperature',  '17',          'hm-rpc.0.xxxxxxxxxxxxxx.1',     '',                          '',                          ''];
 
 // Datenpunkt von HomeMatic für die aktuell am Thermostat gemessene Temperatur
 const HM_STATE_ACTUAL = '.ACTUAL_TEMPERATURE';
 
 // Datenpunkt von HomeMatic für die am Thermostat eingestellte Soll-Temperatur
-// const HM_STATE_SET = '.SET_POINT_TEMPERATURE' --> nehmen bei Homematic IP Thermostaten. Bitte am besten prüfen, welcher bei Euch vorhanden ist ('.SET_TEMPERATURE' oder '.SET_POINT_TEMPERATURE')
-const HM_STATE_SET = '.SET_TEMPERATURE';
-
+const HM_STATE_SET = '.SET_POINT_TEMPERATURE';
 
 // Script wie oft ausführen? 
-const SCHEDULE = '*/30 * * * *' // Alle 30 Minuten
+const SCHEDULE = '* */3 * * *' // Alle 3 Stunden
 
 // Logeinträge auf Debug setzen?
 const DEBUG = true;
@@ -81,7 +83,7 @@ function updateThermostats() {
             var loopHmSetTemp = getState(loopElement + HM_STATE_SET).val;
             var loopHmActualTemp = getState(loopElement + HM_STATE_ACTUAL).val;
             var loopExtTemp = getState(roomsHardware[i][1]).val;
-            var loopOffset = (loopExtTemp - loopHmActualTemp);
+            var loopOffset = (loopExtTemp - loopHmActualTemp)
             var tempArr = loopElement.split(".");
             var loopHmState = tempArr[0] + '.' + tempArr[1]; // Wir brauchen nur den Anfang des States, also von 'hm-rpc.0.ABCDEFGHJ.4' den Teil "hm-rpc.0"
             var loopHmID = tempArr[2]; // Wir brauchen nur die ID, also von 'hm-rpc.0.ABCDEFGHJ.4' den Teil "ABCDEFGHJ"
@@ -95,12 +97,11 @@ function updateThermostats() {
             } else {
                 // Gleich oder überhalb der Minimum-Soll-Temperatur, also machen wir weiter.
                 if(DEBUG) log(lRoom + ': Temp Homematic: ' + loopHmActualTemp + ', Temp Externer Sensor: ' + loopExtTemp);
-                if(DEBUG) log(lRoom + ': Offset: ' + loopOffset + '°C, (gerundet: ' + (Math.round(loopOffset * 2) / 2) + '°C) entspricht Homematic Wert von ' + convertTemperatureToHMvalue(loopOffset));
+                if(DEBUG) log(lRoom + ': Offset: ' + loopOffset + '°C, entspricht für Homematic gerundet: ' + convertTemperatureToHMvalue(loopOffset) + '°C');
                 // Nun setzen wir das neue HomeMatic Offset
-                var offSetHomeMaticValue = convertTemperatureToHMvalue(loopOffset)
-                var result = setHomematicOffset(loopHmState, loopHmID, offSetHomeMaticValue);
+                var result = setHomematicOffset(loopHmState, loopHmID, convertTemperatureToHMvalue(loopOffset));
                 if (isEmpty(result)) {
-                    log(lRoom + ': Neues Offset von ' + convertHMvalueToTemperature(offSetHomeMaticValue) + '°C erfolgreich gesetzt.');
+                    log(lRoom + ': Neues Offset von ' + convertTemperatureToHMvalue(loopOffset) + '°C erfolgreich gesetzt.');
                 } else {
                     log(lRoom + ': Fehler beim Offset-setzen aufgetreten: ' + result, 'warn');
                 }
@@ -109,7 +110,7 @@ function updateThermostats() {
     }
 }
 
-
+ 
 
 /**
  * Setzt für das gegebene Homematic-Thermostat ein neues Offset
@@ -119,7 +120,7 @@ function updateThermostats() {
  * @return String with error message, or empty string if no error occurred.
  */
 function setHomematicOffset(strState, strID, offsetVal) {
-    sendTo(strState, 'putParamset', {ID: strID, paramType: 'MASTER', params: {'TEMPERATURE_OFFSET': offsetVal}}, res => {
+    sendTo(strState, 'putParamset', {ID: strID + ':1', paramType: 'MASTER', params: {'TEMPERATURE_OFFSET': offsetVal}}, res => {
         var errorResult = res['error'];
         if (isEmpty(errorResult)) {
             return '';
@@ -129,19 +130,10 @@ function setHomematicOffset(strState, strID, offsetVal) {
     });
 }
 
-
-/*
-// *READ* the Offset from Homematic
-sendTo('hm-rpc.0', 'getParamset', {ID: 'XXXXXXXXXX', paramType: 'MASTER'}, res => {
-    //log(JSON.stringify(res));
-    log(res["result"]['TEMPERATURE_OFFSET']);
-});
-*/
-
  /**
- * Converts an actual temperature number into Homematic value.
+ * Converts an actual temperature number into valid Homematic value.
  * @param {number}   tempInput   Temperature to convert
- * @return {number}  HomeMatic value. Example: +2.5 will result in 12
+ * @return {number}  target value. Example: -2.655 will result in -2.5
  */
 function convertTemperatureToHMvalue(tempInput) {
     
@@ -152,33 +144,9 @@ function convertTemperatureToHMvalue(tempInput) {
     if (tempInp > 3.5) tempInp = 3.5;
     if (tempInp < -3.5) tempInp = -3.5;
     
-    // Now we calculate the Homematic value, the rule is as follows:
-    // -3,5° | -3,0° | -2,5° | -2,0° | -1,5° | -1,0° | -0,5° | 0,0° | +0,5° | +1,0° | +1,5° | +2,0° | +2,5° | +3,0° | +3,5°
-    //   0       1       2       3       4        5       6     7       8       9      10       11      12     13      14
-    return (tempInp * 2) + 7;
+    return tempInp;
 
 }
-
-/**
- * Converts a Homematic value to actual temperature
- * @param {number}   valueInput   Value to convert
- * @return {number}  Temperature  Example: 12 will result in +2.5
- */
-function convertHMvalueToTemperature(valueInput) {
-
-    // Round, just in case
-    var valInp = Math.round(valueInput);
-
-    // Homematic does not accept values lower than 0 or higher than 14
-    if (valInp > 14) valInp = 14;
-    if (valInp < 0) valInp = 0;
-    
-    // Now we calculate the Homematic value, the rule is as follows:
-    // -3,5° | -3,0° | -2,5° | -2,0° | -1,5° | -1,0° | -0,5° | 0,0° | +0,5° | +1,0° | +1,5° | +2,0° | +2,5° | +3,0° | +3,5°
-    //   0       1       2       3       4        5       6     7       8       9      10       11      12     13      14
-    return (valInp - 7) / 2;
-}
-
 
 /**
  * Get all thermostats of the room into a new array. 
@@ -215,3 +183,12 @@ function isEmpty(inputVar) {
         return true;
     }
 }
+
+
+/*
+// *READ* the Offset from Homematic
+sendTo('hm-rpc.0', 'getParamset', {ID: 'xxxxxxxxxxxxxx:1', paramType: 'MASTER'}, res => {
+    log(JSON.stringify(res));
+    log('XXX: ' + res["result"]['TEMPERATURE_OFFSET']);
+});
+*/
